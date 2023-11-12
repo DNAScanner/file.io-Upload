9792b2ffc7562ea0696b0d4ec2314435b9aa7860
DNAScanner <75182303+DNAScanner@users.noreply.github.com>
2023-11-12 18:37:41 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# file.io Uploader by DNA
+This is a simple deno script, which allows you to upload files easily using just your terminal
+
+## Usage
+- `upload.exe <file>` - Uploads the file to file.io and returns the link
---
+++ main.ts
@@ -0,0 +1,50 @@
+interface FileIOResponse {
+	success: boolean;
+	status: number;
+	id: string;
+	key: string;
+	path: string;
+	nodeType: string;
+	name: string;
+	title: string | null;
+	description: string | null;
+	size: number;
+	link: string;
+	private: boolean;
+	expires: string;
+	downloads: number;
+	maxDownloads: number;
+	autoDelete: boolean;
+	planId: number;
+	screeningStatus: string;
+	mimeType: string;
+	created: string;
+	modified: string;
+}
+
+const filename = Deno.args[0] || "";
+
+if (!filename) {
+	console.error("No filename provided");
+	Deno.exit(1);
+}
+
+try {
+	await Deno.stat(filename);
+} catch {
+	console.error("File not found");
+	Deno.exit(1);
+}
+
+const formData = new FormData();
+formData.append("file", new Blob([await Deno.readFile(filename)]), filename);
+
+// Use fetch to upload the file to file.io
+const response = await(
+	await fetch("https://file.io", {
+		method: "POST",
+		body: formData,
+	})
+).json() as FileIOResponse
+
+console.log(`Uploaded to \x1b[33m${response.link}\x1b[0m`);
